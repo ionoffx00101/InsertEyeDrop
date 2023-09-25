00dc147b101befc6d4d93166642fee3db40aee37
inyeong hwang <ionoffx@gmail.com>
2023-09-25 13:55:41 +0900

    Initial Commit

---
+++ EyesTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EyesTimer
+//
+//  Created by 탐코퍼레이션 iMac2 on 9/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EyesTimer/EyesTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyesTimerApp.swift
+//  EyesTimer
+//
+//  Created by 탐코퍼레이션 iMac2 on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct EyesTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
