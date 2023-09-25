92b36604315c8d943df0d0419834eacbd0f43688
inyeong <ionoffx@gmail.com>
2023-09-25 15:16:30 +0900

    feat: 타이머 기능 추가
    # feat : 새로운 기능 추가
    # fixed : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    # refactor : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    #
    # 제목을 50글자 내로 제한
    # 제목 끝에 마침표 넣지 않기
    # 제목은 명령문으로 사용하며 과거형을 사용하지 않는다
    # 본문의 각 행은 72글자 내로 제한
    # 어떻게 보다는 무엇과 왜를 설명한다

--- EyesTimer/ContentView.swift
+++ EyesTimer/ContentView.swift
@@ -5,17 +5,98 @@
 //  Created by 탐코퍼레이션 iMac2 on 9/25/23.
 //
 
+import AudioToolbox
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isATimerRunning = false
+    @State private var isBTimerRunning = false
+    @State private var aElapsedTime: TimeInterval = 0
+    @State private var bElapsedTime: TimeInterval = 0
+    @State private var aTimer: Timer? = nil
+    @State private var bTimer: Timer? = nil
+
+    private var aTimerTime: Double = 30 * 60
+    private var bTimerTime: Double = 2 * 60 * 60
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            VStack {
+                Text("인공 눈물: \(formattedTime(aTimerTime - aElapsedTime))")
+                Button(action: {
+                    toggleATimer()
+                }) {
+                    Text(isATimerRunning ? "정지 및 초기화" : "시작")
+                }
+            }
+            Spacer()
+            VStack {
+                Text("항생제 / 근시퇴행 안약: \(formattedTime(bTimerTime - bElapsedTime))")
+                Button(action: {
+                    toggleBTimer()
+                }) {
+                    Text(isBTimerRunning ? "정지 및 초기화" : "시작")
+                }
+            }
+            Spacer()
         }
-        .padding()
+    }
+
+    func toggleATimer() {
+        isATimerRunning.toggle()
+
+        if isATimerRunning {
+            aTimer?.invalidate()
+            aTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                if aElapsedTime < aTimerTime {
+                    aElapsedTime += 1
+                } else {
+                    aTimer?.invalidate()
+                    isATimerRunning = false
+                }
+                if Int(aElapsedTime) % Int(aTimerTime) == 0 {
+                    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+                    aElapsedTime = 0
+                }
+            }
+            aTimer?.fire()
+        } else {
+            aTimer?.invalidate()
+            aElapsedTime = 0
+        }
+    }
+
+    func toggleBTimer() {
+        isBTimerRunning.toggle()
+
+        if isBTimerRunning {
+            bTimer?.invalidate()
+            bTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                if bElapsedTime < bTimerTime {
+                    bElapsedTime += 1
+                } else {
+                    bTimer?.invalidate()
+                    isBTimerRunning = false
+                }
+                if Int(bElapsedTime) % Int(bTimerTime) == 0 {
+                    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+                    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+                    bElapsedTime = 0
+                }
+            }
+            bTimer?.fire()
+        } else {
+            bTimer?.invalidate()
+            bElapsedTime = 0
+        }
+    }
+
+    func formattedTime(_ time: TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = (Int(time) % 3600) / 60
+        let seconds = Int(time) % 60
+        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
     }
 }
 
